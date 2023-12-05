29dcb5f9facffd85e8aab881e47bc28cce48e84e
Tae Kimura <sheepman7893@gmail.com>
2023-12-04 19:31:25 -0800

    fix: adjust loading/card styles

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -18,12 +18,12 @@ export default function Card({ result }: any) {
             height: 'auto'
           }}
           placeholder='blur'
-          blurDataURL='/spinner.svg'
+          blurDataURL='/loading.svg'
           alt='image is not available'
         ></Image>
         <div className='p-2'>
           <p className='line-clamp-2 text-md'>{result.overview}</p>
-          <h2 className='truncate text-lg font-bold'>
+          <h2 className='line-clamp-1 text-lg font-bold'>
             {result.title || result.name}
           </h2>
           <p className='flex items-center'>
