4903e5c21f1278797d98c52071eaff99cb0bfdb0
Tae Kimura <sheepman7893@gmail.com>
2023-12-04 18:20:29 -0800

    feat: fetch API and add card component

---
+++ src/app/components/Results.tsx
@@ -0,0 +1,14 @@
+import Card from './Card';
+import { Result } from '../types';
+
+type ResultProps = { results: Result[] };
+
+export default function Results({ results }: ResultProps) {
+  return (
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
+      {results.map((result: Result) => (
+        <Card key={result.id} result={result} />
+      ))}
+    </div>
+  );
+}
