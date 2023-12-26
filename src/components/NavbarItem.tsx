ee1bc08e4d0cba9f589eb4de3fe3a345d26a4b4f
Tae Kimura <sheepman7893@gmail.com>
2023-12-26 02:40:33 -0800

    refactor: add an underline to the default genre

--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -13,12 +13,12 @@ export default function NavbarItem({
 }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get('genre');
+
   return (
     <div>
       <Link
         className={`m-4 hover:text-amber-600 font-semibold p-2 ${
-          genre &&
-          genre === param &&
+          ((!genre && title === 'Trending') || genre === param) &&
           'underline underline-offset-8 decoration-4  decoration-amber-500 rounded-lg'
         }`}
         href={`/?genre=${param}`}
